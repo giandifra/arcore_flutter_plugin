1991c6527cb845ea91c067ef3c81f365b9b0c028
Vladi Rachlin <vladi.r95@gmail.com>
2019-12-14 19:51:13 +0200

    fix onAttachedToEngine

--- android/src/main/kotlin/com/difrancescogianmarco/arcore_flutter_plugin/ArcoreFlutterPlugin.kt
+++ android/src/main/kotlin/com/difrancescogianmarco/arcore_flutter_plugin/ArcoreFlutterPlugin.kt
@@ -1,8 +1,12 @@
 package com.difrancescogianmarco.arcore_flutter_plugin
 
-import android.util.Log
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class ArcoreFlutterPlugin: FlutterPlugin{
 
@@ -10,13 +14,16 @@ class ArcoreFlutterPlugin: FlutterPlugin{
 
         val TAG = "ArCoreFlutterPlugin"
         @JvmStatic
-        fun registerWith(registrar: PluginRegistry.Registrar) {
+        fun registerWith(registrar: Registrar) {
             registrar
                     .platformViewRegistry()
                     .registerViewFactory("arcore_flutter_plugin", ArCoreViewFactory(registrar.messenger()))
         }
     }
 
-    override fun onAttachedToEngine (p0: FlutterPlugin.FlutterPluginBinding) {}
-    override fun onDetachedFromEngine (p0: FlutterPlugin.FlutterPluginBinding) {}
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "arcore_flutter_plugin")
+        flutterPluginBinding.getPlatformViewRegistry().registerViewFactory("arcore_flutter_plugin", ArCoreViewFactory(flutterPluginBinding.getBinaryMessenger()))
+    }
+      override fun onDetachedFromEngine (p0: FlutterPlugin.FlutterPluginBinding) {}
 }
